2b9d36bf183188c60bfbab790d060da006f200ba
FourtyThree43 <shaqmwa@outlook.com>
2023-11-02 22:25:39 +0300

    The last time I tried this the monkey didn't survive. Let's hope it works better this time.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -37,7 +37,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "holberton")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "my_db")
     db_port = os.getenv('PERSONAL_DATA_DB_PORT', 3306)  # Default MySQL port
 
     connector = None
