82ddc029d3d6806b7957a0fce5c731b236cb7d6b
FourtyThree43 <shaqmwa@outlook.com>
2023-11-07 22:39:52 +0300

    More ignore

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -20,17 +20,40 @@ class BasicAuth(Auth):
             return None
         return authorization_header[6:]
 
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Method that returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return base64_authorization_header.encode('utf-8').decode('base64')
+        except Exception:
+            return None
+
 
 if __name__ == "__main__":
 
     a = BasicAuth()
 
-    print(a.extract_base64_authorization_header(None))
-    print(a.extract_base64_authorization_header(89))
-    print(a.extract_base64_authorization_header("Holberton School"))
-    print(a.extract_base64_authorization_header("Basic Holberton"))
-    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+    # print(a.extract_base64_authorization_header(None))
+    # print(a.extract_base64_authorization_header(89))
+    # print(a.extract_base64_authorization_header("Holberton School"))
+    # print(a.extract_base64_authorization_header("Basic Holberton"))
+    # print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+    # print(
+    #     a.extract_base64_authorization_header(
+    #         "Basic SG9sYmVydG9uIFNjaG9vbA=="))
+    # print(a.extract_base64_authorization_header("Basic1234"))
+
+    print(a.decode_base64_authorization_header(None))
+    print(a.decode_base64_authorization_header(89))
+    print(a.decode_base64_authorization_header("Holberton School"))
+    print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
+    print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
     print(
-        a.extract_base64_authorization_header(
-            "Basic SG9sYmVydG9uIFNjaG9vbA=="))
-    print(a.extract_base64_authorization_header("Basic1234"))
+        a.decode_base64_authorization_header(
+            a.extract_base64_authorization_header(
+                "Basic SG9sYmVydG9uIFNjaG9vbA==")))
