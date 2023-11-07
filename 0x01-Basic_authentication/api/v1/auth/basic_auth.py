6544c6b3e09dcce9ed2d334d460bdf79a8525bba
FourtyThree43 <shaqmwa@outlook.com>
2023-11-07 22:34:57 +0300

    "Get that shit outta my master."

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,30 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Class of Basic auth
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Method that returns the Base64 part of the Authorization header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
+
+
+if __name__ == "__main__":
+
+    a = BasicAuth()
+
+    print(a.extract_base64_authorization_header(None))
+    print(a.extract_base64_authorization_header(89))
+    print(a.extract_base64_authorization_header("Holberton School"))
+    print(a.extract_base64_authorization_header("Basic Holberton"))
+    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+    print(
+        a.extract_base64_authorization_header(
+            "Basic SG9sYmVydG9uIFNjaG9vbA=="))
+    print(a.extract_base64_authorization_header("Basic1234"))
