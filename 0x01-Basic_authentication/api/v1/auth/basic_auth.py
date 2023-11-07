16219e23607af579dbc4802808bc8c7ec11ff0a5
FourtyThree43 <shaqmwa@outlook.com>
2023-11-07 22:50:06 +0300

    Stuff

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of Basic auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -29,7 +30,8 @@ class BasicAuth(Auth):
         if not isinstance(base64_authorization_header, str):
             return None
         try:
-            return base64_authorization_header.encode('utf-8').decode('base64')
+            return base64.b64decode(
+                base64_authorization_header.encode('utf-8')).decode('utf-8')
         except Exception:
             return None
 
