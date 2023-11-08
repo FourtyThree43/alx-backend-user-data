0c1a1dfeba824085c7c92e26e739820f707cb5a3
FourtyThree43 <shaqmwa@outlook.com>
2023-11-08 22:33:10 +0300

    giggle.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 """ Module of Session Auth
 """
+from typing import Dict
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Auth class
     """
-    pass
+    user_id_by_session_id: Dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create session
+        """
+        if user_id is None or isinstance(user_id, str) is False:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
