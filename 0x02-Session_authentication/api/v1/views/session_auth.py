7d63beb5d311c0966b129110e2401f364805da72
FourtyThree43 <shaqmwa@outlook.com>
2023-11-09 13:50:31 +0300

    Improving the fix

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -8,22 +8,22 @@ from os import getenv
 from typing import Tuple, Union
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
 def login() -> Union[Tuple[Response, int], Response]:
     """ POST /api/v1/auth_session/login
     Return:
       - User object JSON represented
     """
-    email = request.form.get('email')
-    if not email or email == '':
+    email = request.form.get("email")
+    if not email or email == "":
         return jsonify({"error": "email missing"}), 400
 
-    password = request.form.get('password')
-    if not password or password == '':
+    password = request.form.get("password")
+    if not password or password == "":
         return jsonify({"error": "password missing"}), 400
 
     try:
-        user = User.search({'email': email})
+        user = User.search({"email": email})
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
 
@@ -34,7 +34,7 @@ def login() -> Union[Tuple[Response, int], Response]:
         from api.v1.app import auth
 
         session_id = auth.create_session(user[0].id)
-        cookie_name = getenv('SESSION_NAME')
+        cookie_name = getenv("SESSION_NAME")
         response = jsonify(user[0].to_json())
         response.set_cookie(cookie_name, session_id)
 
