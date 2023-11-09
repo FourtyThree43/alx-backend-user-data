5c0b81e220cbf2fcb8b4dcedf05fb3e23bcf440d
FourtyThree43 <shaqmwa@outlook.com>
2023-11-09 14:28:17 +0300

    Fixed the fuck out of #776!

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,42 +2,42 @@
 """Module of session authenticating views.
 """
 import os
-from typing import Tuple, Union
-from flask import Response, abort, jsonify, request
+from typing import Tuple
+from flask import abort, jsonify, request
 
 from models.user import User
 from api.v1.views import app_views
 from models.user import User
 
 
-@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
-def login() -> Union[Tuple[Response, int], Response]:
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> Tuple[str, int]:
     """ POST /api/v1/auth_session/login
     Return:
       - User object JSON represented
     """
     email = request.form.get("email")
-    if not email or email == "":
+    if email is None or email == "":
         return jsonify({"error": "email missing"}), 400
 
     password = request.form.get("password")
-    if not password or password == "":
+    if password is None or password == "":
         return jsonify({"error": "password missing"}), 400
 
     try:
-        user = User.search({"email": email})
+        users = User.search({"email": email})
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
 
-    if not user:
+    if len(users) <= 0:
         return jsonify({"error": "no user found for this email"}), 404
 
-    if user[0].is_valid_password(password):
+    if users[0].is_valid_password(password):
         from api.v1.app import auth
 
-        session_id = auth.create_session(user[0].id)
+        session_id = auth.create_session(getattr(users[0], 'id'))
         cookie_name = os.getenv("SESSION_NAME")
-        response = jsonify(user[0].to_json())
+        response = jsonify(users[0].to_json())
         response.set_cookie(cookie_name, session_id)
 
         return response
