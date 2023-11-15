0fb86ec335941e632c729f3c5920520b589b26e8
FourtyThree43 <shaqmwa@outlook.com>
2023-11-15 13:33:47 +0300

    Code was clean until manager requested to fuck it up

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Auth Module
 """
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
@@ -8,3 +10,21 @@ def _hash_password(password: str) -> bytes:
     """ Returns the hashed version of a password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> None:
+        """ Registers a user
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError(f'User {email} already exists')
