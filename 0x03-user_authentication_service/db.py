fd96bef406e6715772f827a25999fe109af9a759
FourtyThree43 <shaqmwa@outlook.com>
2023-11-15 12:45:17 +0300

    it is hump day _^_

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -18,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -41,7 +41,7 @@ class DB:
             self._session.commit()
         except Exception:
             self._session.rollback()
-            raise
+            new_user = None
 
         return new_user
 
