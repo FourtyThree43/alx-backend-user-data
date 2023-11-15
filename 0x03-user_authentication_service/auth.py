d5100a1dc4a6f8a33785412235c32091fc6c4b73
FourtyThree43 <shaqmwa@outlook.com>
2023-11-15 15:14:27 +0300

    ¯\_(ツ)_/¯

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ Auth Module
 """
-from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
+
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -17,7 +19,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> None:
+    def register_user(self, email: str, password: str) -> User:
         """Registers a user"""
         try:
             self._db.find_user_by(email=email)
