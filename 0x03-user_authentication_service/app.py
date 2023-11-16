d04de86a20f611f04c208ef8b7cbb2c316a80689
FourtyThree43 <shaqmwa@outlook.com>
2023-11-16 12:26:18 +0300

    WTF is this.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -33,5 +33,21 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    """POST /sessions
+    Return:
+        - The session ID as a JSON payload.
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Auth Module
 """
+from typing import Optional
 from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 import uuid
@@ -57,3 +58,16 @@ class Auth:
             self._db.update_user(user.id, session_id=session_id)
 
             return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Optional[str]:
+        """ Get user from session ID
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        else:
+            return user
