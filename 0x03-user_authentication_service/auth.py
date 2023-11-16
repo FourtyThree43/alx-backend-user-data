fd81a85cb57a835ccdf9bef3fbe336efd1472208
FourtyThree43 <shaqmwa@outlook.com>
2023-11-16 12:15:35 +0300

    wip

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,3 +44,16 @@ class Auth:
         else:
             return bcrypt.checkpw(password.encode("utf-8"),
                                   user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """ Create a session ID for the user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        else:
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+
+            return session_id
